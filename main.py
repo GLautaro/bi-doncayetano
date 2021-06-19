19c0204a0c415d7dce690707c7ff3f76aba50d9b
agustinch <chg.agustin@gmail.com>
2021-06-19 13:28:52 -0300

    New environment Database connection

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import psycopg2
+from sqlalchemy import create_engine
+
+alchemyEngine=create_engine(f"postgresql+psycopg2://{st.secrets['user']}:{st.secrets['password']}@{st.secrets['host']}/{st.secrets['db']}", pool_recycle=3600)
+dbConnection=alchemyEngine.connect()
+
+
+dataFrame=pd.read_sql("select * from hechos_compras hc join fechas_solicitudes fs on fs.id_fecha_solicitud = hc.id_fecha_solicitud ", dbConnection)
+
+pd.set_option('display.expand_frame_repr', False);
+
+
+dataFrame = dataFrame.groupby(by="nombre_mes").sum()
+
+
+dbConnection.close()
 
 st.title('BI - Don Cayetanos')
 
 
 st.text('Desarrollo de los requerimientos solicitados')
+st.dataframe(dataFrame)
