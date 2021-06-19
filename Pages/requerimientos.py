b7c61422e2a4e9a12179ad6621f99b7a96d3aa46
agustinch <chg.agustin@gmail.com>
2021-06-19 16:54:13 -0300

    Add requerimientos

--- Pages/requerimientos.py
+++ Pages/requerimientos.py
@@ -1,6 +1,38 @@
 import streamlit as st
 
-def LoadPage(dataset):
+def LoadPage(df):
     st.title("Requerimientos")
     st.write("Set de datos:")
-    st.write(dataset)
+    st.write(df)
+    st.header('Requerimiento 1 - Porcentajes de ingredientes solicitados por mes/año')
+    #Requerimiento 1 - Mes
+    st.write('Por mes')
+    df_r1_mes = df.loc[df['id_tipo_producto'] == 1]
+    df_r1_mes = df_r1_mes.groupby(['nombre_mes_solicitud', 'nombre_producto']).agg({'cantidad_solicitada': 'sum'})
+    df_r1_mes['Porcentaje'] = df_r1_mes.groupby(level=0).apply(lambda x:100 * x / float(x.sum()))
+    st.write(df_r1_mes)
+    #Requerimiento 1 - Anio
+    st.write('Por año')
+    df_r1_anio = df.loc[df['id_tipo_producto'] == 1]
+    df_r1_anio = df_r1_anio.groupby(['anio_solicitud', 'nombre_producto']).agg({'cantidad_solicitada': 'sum'})
+    df_r1_anio['Porcentaje'] = df_r1_anio.groupby(level=0).apply(lambda x:100 * x / float(x.sum()))
+    st.write(df_r1_anio)
+    st.header('Requerimiento 2')
+    #Requerimiento 2  - Mes
+    st.write('Por mes')
+    df_r2_mes = df.loc[df['id_tipo_producto'] == 1]
+    df_r2_mes = df_r2_mes.groupby(['nombre_producto', 'nombre_proveedor', 'nombre_mes_solicitud']).agg({'cantidad_solicitada': 'sum', 'cantidad_recibida':'sum'})
+    st.write(df_r2_mes)
+    #Requerimiento 2  - Anio
+    st.write('Por año')
+    df_r2_anio = df.loc[df['id_tipo_producto'] == 1]
+    df_r2_anio = df_r2_anio.groupby(['nombre_producto', 'nombre_proveedor', 'anio_solicitud']).agg({'cantidad_solicitada': 'sum', 'cantidad_recibida':'sum'})
+    st.write(df_r2_anio)
+    st.header('Requerimiento 3')
+    #Requerimiento 3 - Mes
+    st.write('Por mes')
+    df_r3_mes = df.groupby(['nombre_mes_solicitud', 'nombre_proveedor']).agg({'cantidad_solicitada': 'sum','cantidad_recibida': 'sum'})
+    df_r3_mes['cantidad_perdida'] =  df_r3_mes['cantidad_solicitada'] - df_r3_mes['cantidad_recibida']
+    df_r3_mes['porcentaje_perdida'] = df_r3_mes.apply(lambda x:100 * x.cantidad_perdida / x.cantidad_solicitada,axis=1)
+    st.write(df_r3_mes)
+ 
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import Pages.requerimientos as requerimientos
 alchemyEngine=create_engine(f"postgresql+psycopg2://{st.secrets['user']}:{st.secrets['password']}@{st.secrets['host']}/{st.secrets['dbname']}", pool_recycle=3600)
 dbConnection=alchemyEngine.connect()
 
-dataFrame=pd.read_sql("select * from hechos_compras hc join fechas_solicitudes fs on fs.id_fecha_solicitud = hc.id_fecha_solicitud ", dbConnection)
+df=pd.read_sql("select prod.id_tipo_producto, prod.nombre_tipo_producto, prod.nombre as nombre_producto, prov.nombre as nombre_proveedor, fs.nombre_mes as nombre_mes_solicitud, fs.anio as anio_solicitud, fr.nombre_mes as nombre_mes_recepcion, fr.anio as anio_recepcion, cantidad_solicitada, cantidad_recibida from hechos_compras hc join fechas_solicitudes fs on fs.id_fecha_solicitud = hc.id_fecha_solicitud join fechas_recepciones fr on fr.id_fecha_recepcion = hc.id_fecha_recepcion join productos prod on prod.id_producto = hc.id_producto join proveedores prov on prov.id_proveedor = hc.id_proveedor", dbConnection)
 pd.set_option('display.expand_frame_repr', False);
 
 ##dataFrame = dataFrame.groupby(by="nombre_mes").sum()
@@ -27,7 +27,7 @@ def CreateLayout():
     if app_mode == 'Introducción':
         introduccion.LoadPage()
     elif app_mode == 'Requerimientos':
-        requerimientos.LoadPage(dataFrame)
+        requerimientos.LoadPage(df)
     else:
         pass
 
