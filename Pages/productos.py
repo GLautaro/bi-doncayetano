3cc3e01feec92391d5820b68486bff443c70fd73
agustinch <chg.agustin@gmail.com>
2021-06-20 16:10:03 -0300

    Requerimiento 5 y 9

--- Pages/productos.py
+++ Pages/productos.py
@@ -58,13 +58,59 @@ def requerimiento_2(dataset, meses):
     df_r2_anio = df_r2_anio.groupby(['nombre_producto', 'nombre_proveedor']).agg({'cantidad_solicitada': 'sum', 'cantidad_recibida':'sum'})
     st.write(df_r2_anio)
 
+def requerimiento_5(dataset):
+    st.header('5 y 9 - Promedio de compras de productos por mes')
+    st.write('Frecuencia de compra')
+    tipo_producto= st.selectbox(key="tipo_producto_r5",
+     label='Tipo Producto',
+    options=('Ingrediente', 'Packaging', 'Insumo descartable'))
+    anio = st.selectbox(key="anio_r5",
+     label='Año',
+    options=(2019,2020,2021, 'Todos los años'))
+    df=dataset.loc[dataset['nombre_tipo_producto'] == tipo_producto]
+    if anio != 'Todos los años':
+        df = df.loc[df['anio_solicitud'] == anio]
+        df = df.groupby(['mes_solicitud','nombre_mes_solicitud','nombre_producto']).agg({'id_producto': 'count'})
+        df = df.sort_values(['mes_solicitud'])
+        df = df.reset_index()
+        fig = px.bar(df, x='nombre_mes_solicitud', y='id_producto', color="nombre_producto")
+    else: 
+        df = df.groupby(['anio_solicitud','nombre_producto']).agg({'id_producto': 'count'})
+        df = df.reset_index()
+        df =df.astype({"anio_solicitud": str})
+        fig = px.bar(df, x='anio_solicitud', y='id_producto', color="nombre_producto")
+    st.write(fig)
+    
+    st.write('Promedio de cantidad comprada')
+    tipo_producto= st.selectbox(key="tipo_producto_r5_prom",
+     label='Tipo Producto',
+    options=('Ingrediente', 'Packaging', 'Insumo descartable'))
+    anio = st.selectbox(key="anio_r5_prom",
+     label='Año',
+    options=(2019,2020,2021, 'Todos los años'))
+    df=dataset.loc[dataset['nombre_tipo_producto'] == tipo_producto]
+    if anio != 'Todos los años':
+        df = df.loc[df['anio_solicitud'] == anio]
+        df = df.groupby(['mes_solicitud','nombre_mes_solicitud','nombre_producto']).agg({'cantidad_solicitada': 'mean'})
+        df = df.sort_values(['mes_solicitud'])
+        df = df.reset_index()
+        fig = px.bar(df, x='nombre_mes_solicitud', y='cantidad_solicitada', color="nombre_producto")
+    else: 
+        df = df.groupby(['anio_solicitud','nombre_producto']).agg({'cantidad_solicitada': 'mean'})
+        df = df.reset_index()
+        df =df.astype({"anio_solicitud": str})
+        fig = px.bar(df, x='anio_solicitud', y='cantidad_solicitada', color="nombre_producto")
+    st.write(fig)
+
+
+
 
 def LoadPage(dataset):
     meses = ['Enero', 'Febrero', 'Marzo','Abril', 'Mayo','Junio','Julio','Agosto','Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ]
     
     requerimiento_1(dataset)
 
-    requerimiento_2(dataset,meses)
+    requerimiento_2(dataset, meses)
 
-
+    requerimiento_5(dataset)
     
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import Pages.productos as productos
 alchemyEngine=create_engine(f"postgresql+psycopg2://{st.secrets['user']}:{st.secrets['password']}@{st.secrets['host']}/{st.secrets['dbname']}", pool_recycle=3600)
 dbConnection=alchemyEngine.connect()
 
-df=pd.read_sql("select prod.id_tipo_producto, prod.nombre_tipo_producto, prod.nombre as nombre_producto, prov.nombre as nombre_proveedor, fs.nombre_mes as nombre_mes_solicitud,fs.mes as mes_solicitud, fs.anio as anio_solicitud, fr.nombre_mes as nombre_mes_recepcion, fr.mes as mes_recepcion, fr.anio as anio_recepcion, cantidad_solicitada, cantidad_recibida from hechos_compras hc join fechas_solicitudes fs on fs.id_fecha_solicitud = hc.id_fecha_solicitud join fechas_recepciones fr on fr.id_fecha_recepcion = hc.id_fecha_recepcion join productos prod on prod.id_producto = hc.id_producto join proveedores prov on prov.id_proveedor = hc.id_proveedor", dbConnection)
+df=pd.read_sql("select hc.id_compra, prod.id_tipo_producto, prod.nombre_tipo_producto, prod.id_producto, prod.nombre as nombre_producto, prov.nombre as nombre_proveedor, fs.nombre_mes as nombre_mes_solicitud,fs.mes as mes_solicitud, fs.anio as anio_solicitud, fr.nombre_mes as nombre_mes_recepcion, fr.mes as mes_recepcion, fr.anio as anio_recepcion, cantidad_solicitada, cantidad_recibida from hechos_compras hc join fechas_solicitudes fs on fs.id_fecha_solicitud = hc.id_fecha_solicitud join fechas_recepciones fr on fr.id_fecha_recepcion = hc.id_fecha_recepcion join productos prod on prod.id_producto = hc.id_producto join proveedores prov on prov.id_proveedor = hc.id_proveedor", dbConnection)
 pd.set_option('display.expand_frame_repr', False);
 
 ##dataFrame = dataFrame.groupby(by="nombre_mes").sum()
