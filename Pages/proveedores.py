32ca99a10f5e550d6c8216461561be811e73830c
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2021-06-20 18:43:05 -0300

    Correccion labels

--- Pages/proveedores.py
+++ Pages/proveedores.py
@@ -14,20 +14,20 @@ def LoadPage(df):
     st.header("Comparador por condicion de pago")
     proveedores = st.multiselect("Seleccione proveedores", list(df["nombre_proveedor"].unique()), ["Dahyana Azabal", "Agustin Chiavassa"])
 
-    df_r4 = df[["nombre_proveedor", "condicion_pago"]].drop_duplicates()
+    df_r6 = df[["nombre_proveedor", "condicion_pago"]].drop_duplicates()
     
-    st.write(df_r4[df_r4["nombre_proveedor"].isin(proveedores)])
+    st.write(df_r6[df_r6["nombre_proveedor"].isin(proveedores)])
 
     st.header("Comparador por Tiempo de Entrega por producto")
     proveedores_2 = st.multiselect("Seleccione proveedores", list(df["nombre_proveedor"].unique()), ["Dahyana Azabal", "Lautaro Gonzalez"])
-    productos = st.multiselect("Seleccione proveedores", list(df["nombre_producto"].unique()), ["Levadura", "Harina 0000"])
+    productos = st.multiselect("Seleccione productos", list(df["nombre_producto"].unique()), ["Levadura", "Harina 0000"])
 
-    df_r5 = df[["nombre_proveedor", "nombre_producto", "fecha_solicitud", "fecha_recepcion"]]
+    df_r7 = df[["nombre_proveedor", "nombre_producto", "fecha_solicitud", "fecha_recepcion"]]
 
-    df_r5['tiempo_entrega'] =  df_r5['fecha_recepcion'] - df_r5['fecha_solicitud']
-    df_r5['tiempo_entrega'] =  df_r5['tiempo_entrega'] / np.timedelta64(1,"D")
-    df_r5 = df_r5.groupby(['nombre_proveedor', 'nombre_producto']).mean().reset_index()
+    df_r7['tiempo_entrega'] =  df_r7['fecha_recepcion'] - df_r7['fecha_solicitud']
+    df_r7['tiempo_entrega'] =  df_r7['tiempo_entrega'] / np.timedelta64(1,"D")
+    df_r7 = df_r7.groupby(['nombre_proveedor', 'nombre_producto']).mean().reset_index()
 
-    st.write(df_r5[df_r5["nombre_proveedor"].isin(proveedores_2) & df_r5["nombre_producto"].isin(productos)])
+    st.write(df_r7[df_r7["nombre_proveedor"].isin(proveedores_2) & df_r7["nombre_producto"].isin(productos)])
 
 
