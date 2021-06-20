6b6fd5bb03a0c1e9d632c49fdd08080fb98bc4b6
agustinch <chg.agustin@gmail.com>
2021-06-20 19:28:32 -0300

    Error nombre de parametro

--- Pages/proveedores.py
+++ Pages/proveedores.py
@@ -47,17 +47,17 @@ def requerimiento_4(dataset):
     
 def requerimiento_7(dataset):
     st.header("Comparador por condicion de pago")
-    proveedores = st.multiselect("Seleccione proveedores", list(df["nombre_proveedor"].unique()), ["Dahyana Azabal", "Agustin Chiavassa"])
+    proveedores = st.multiselect("Seleccione proveedores", list(dataset["nombre_proveedor"].unique()), ["Dahyana Azabal", "Agustin Chiavassa"])
 
-    df_r6 = df[["nombre_proveedor", "condicion_pago"]].drop_duplicates()
+    df_r6 = dataset[["nombre_proveedor", "condicion_pago"]].drop_duplicates()
     
     st.write(df_r6[df_r6["nombre_proveedor"].isin(proveedores)])
 
     st.header("Comparador por Tiempo de Entrega por producto")
-    proveedores_2 = st.multiselect("Seleccione proveedores", list(df["nombre_proveedor"].unique()), ["Dahyana Azabal", "Lautaro Gonzalez"])
-    productos = st.multiselect("Seleccione productos", list(df["nombre_producto"].unique()), ["Levadura", "Harina 0000"])
+    proveedores_2 = st.multiselect("Seleccione proveedores", list(dataset["nombre_proveedor"].unique()), ["Dahyana Azabal", "Lautaro Gonzalez"])
+    productos = st.multiselect("Seleccione productos", list(dataset["nombre_producto"].unique()), ["Levadura", "Harina 0000"])
 
-    df_r7 = df[["nombre_proveedor", "nombre_producto", "fecha_solicitud", "fecha_recepcion"]]
+    df_r7 = dataset[["nombre_proveedor", "nombre_producto", "fecha_solicitud", "fecha_recepcion"]]
 
     df_r7['tiempo_entrega'] =  df_r7['fecha_recepcion'] - df_r7['fecha_solicitud']
     df_r7['tiempo_entrega'] =  df_r7['tiempo_entrega'] / np.timedelta64(1,"D")
